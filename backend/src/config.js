0b2f8831f2c13271c6de7e34be6e57db14bf1f95
@testinghub01 <oussamabo331@gmail.com>
2022-11-27 11:42:21 +0100

    color

--- backend/src/config.js
+++ backend/src/config.js
@@ -66,7 +66,7 @@ const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the ro
 const ROYALTY_ADDRESS = "0x71dd0BdDa47271bEa36598d860098656cf45f3e9"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = ["0x71dd0BdDa47271bEa36598d860098656cf45f3e9"]; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0x875084FD3cBaa525cAfA49cE384aF49c9820D69f"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
